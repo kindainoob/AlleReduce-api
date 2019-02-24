feab3a8cc7ded43ea0c3c05c47895e520b552afa
TATSUHIRO <>
2019-02-25 01:39:18 +0900

    ユーザー登録　DBへの登録

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationController < ApplicationController
+  def index
+    @name = params['name']
+    @password = params['password']
+    @age = params['age']
+    @sex = params['sex']
+    @allergy = params['allergys']
+    User.create(u_name: @name,password_digest: @password,sex: @sex, age: @age, allergy: @allergy.join(','))
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  post 'registration', to: 'registration#index'
+
   resources :restaurants do
     resources :menus
   end
