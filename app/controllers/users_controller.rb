162bf02e0992ceb367f1ec174d37575525fadb64
TATSUHIRO <>
2019-02-28 03:58:37 +0900

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
       redirect_to 'http://www.inatatsu.com/AlleReduce/login.html'
     end
     # session[:user] = "test4"
-    @user = User.find_by(u_name: session[:user])
+    @user = User.find_by(u_name:session[:user])
+    puts @user.u_name
     render json:@user
   end
 end
